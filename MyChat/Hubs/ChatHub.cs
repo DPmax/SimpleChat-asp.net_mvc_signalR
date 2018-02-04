2dfeb60bcf021564adf98cd36e6c9d5be2abe95c
DPmax <Longcheng.Chi@student.csulb.edu>
2018-02-03 16:15:25 -0800

    signalR
    simple chat

---
+++ MyChat/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message, string currentdatetime)
+        {
+            currentdatetime = DateTime.Now.ToString();
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message,currentdatetime);
+        }
+    }
+}
